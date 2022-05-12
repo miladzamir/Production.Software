3815829d7a813cf7a21524555eb203e2ebb64db3
Milad Dehghan <milad.dehghan@hotmail.com>
2022-05-13 00:26:45 +0430

    Get all product handler

---
+++ Production.Software.Application/Handlers/QueryHandlers/GetAllProductHandler.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Production.Software.Application.Queries;
+using Production.Software.Core.Entities;
+using Production.Software.Core.Repositories;
+
+namespace Production.Software.Application.Handlers.QueryHandlers;
+
+public class GetAllProductHandler : IRequestHandler<GetAllProductQuery, List<Product>>
+{
+    private readonly IProductRepository _productRepository;
+
+    public GetAllProductHandler(IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
+    public async Task<List<Product>> Handle(GetAllProductQuery request, CancellationToken cancellationToken)
+    {
+        return (List<Product>)await _productRepository.GetAllAsync();
+    }
+}
