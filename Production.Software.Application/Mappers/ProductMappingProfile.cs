07b2ff2213b8eb6cecb96eaa406abd00cba196fd
Milad Dehghan <milad.dehghan@hotmail.com>
2022-05-13 00:29:24 +0430

    Product mapper

---
+++ Production.Software.Application/Mappers/ProductMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Production.Software.Application.Commands;
+using Production.Software.Application.Responses;
+using Production.Software.Core.Entities;
+
+namespace Production.Software.Application.Mappers;
+
+public class ProductMappingProfile : Profile
+{
+    public ProductMappingProfile() {
+        CreateMap <Product, ProductResponse>().ReverseMap();
+        CreateMap <Product, CreateProductCommand>().ReverseMap();
+    }
+}
