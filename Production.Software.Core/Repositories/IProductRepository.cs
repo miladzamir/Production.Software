3fa9c0a6e9b76d7410133732526bb77eea2d68d8
Milad Dehghan <milad.dehghan@hotmail.com>
2022-05-13 00:28:17 +0430

    IProductRepository and ProductRepository

---
+++ Production.Software.Core/Repositories/IProductRepository.cs
@@ -0,0 +1,9 @@
+﻿using Production.Software.Core.Entities;
+using Production.Software.Core.Repositories.Base;
+
+namespace Production.Software.Core.Repositories;
+
+public interface IProductRepository : IRepository<Product>
+{
+    
+}
---
+++ Production.Software.Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using Production.Software.Core.Entities;
+using Production.Software.Core.Repositories;
+using Production.Software.Core.Repositories.Base;
+using Production.Software.Infrastructure.Data;
+
+namespace Production.Software.Infrastructure.Repositories;
+
+public class ProductRepository : Repository<Product>, IProductRepository
+{
+    public ProductRepository(AppDbContext appDbContext): base(appDbContext) {}
+}
