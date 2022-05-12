bddeff01ba944c8d5138ed1e0be6af2f1f2ff43e
Milad Dehghan <milad.dehghan@hotmail.com>
2022-05-12 22:17:33 +0430

    Setup

--- Production.Software.API/Program.cs
+++ Production.Software.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Production.Software.Infrastructure.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Add AppDbContext
+builder.Services.AddDbContext<AppDbContext>(m => 
+    m.UseNpgsql(builder.Configuration.GetConnectionString("Default") ?? throw new InvalidOperationException()), ServiceLifetime.Singleton);
 
 var app = builder.Build();
 
