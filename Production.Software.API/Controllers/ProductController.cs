440e9c70781d83a53c878e34573e8f3310ba0948
Milad Dehghan <milad.dehghan@hotmail.com>
2022-05-13 00:30:00 +0430

    Product get and create API

---
+++ Production.Software.API/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Production.Software.Application.Commands;
+using Production.Software.Application.Queries;
+using Production.Software.Application.Responses;
+using Production.Software.Core.Entities;
+
+namespace Production.Software.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    public ProductController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<List<Product>> Get()
+    {
+        return await _mediator.Send(new GetAllProductQuery());
+    }
+    
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<ActionResult<ProductResponse>> Create([FromBody] CreateProductCommand command)
+    {
+        var result = await _mediator.Send(command);
+        return Ok(result);
+    }
+}
