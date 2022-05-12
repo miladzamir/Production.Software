b634d74fc428baa1d809c86b9f4d91fcb4428226
Milad Dehghan <milad.dehghan@hotmail.com>
2022-05-13 00:26:17 +0430

    Create product handler

---
+++ Production.Software.Application/Handlers/CommandHandlers/CreateProductHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Production.Software.Application.Commands;
+using Production.Software.Application.Mappers;
+using Production.Software.Application.Responses;
+using Production.Software.Core.Entities;
+using Production.Software.Core.Repositories;
+
+namespace Production.Software.Application.Handlers.CommandHandlers;
+
+public class CreateProductHandler: IRequestHandler<CreateProductCommand, ProductResponse> 
+{
+    private readonly IProductRepository _productRepository;
+    public CreateProductHandler(IProductRepository productRepository) {
+        _productRepository = productRepository;
+    }
+    public async Task<ProductResponse> Handle(CreateProductCommand request, CancellationToken cancellationToken) {
+        var productEntity = ProductMapper.Mapper.Map<Product>(request);
+        if (productEntity is null) {
+            throw new ApplicationException("Issue with mapper");
+        }
+        var newProduct = await _productRepository.AddAsync(productEntity);
+        var productResponse = ProductMapper.Mapper.Map<ProductResponse>(newProduct);
+        return productResponse;
+    }
+}
