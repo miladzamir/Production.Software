cd315ecabd9bad3271e79cfee53e6444951a88d8
Milad Dehghan <milad.dehghan@hotmail.com>
2022-05-13 00:29:35 +0430

    Register services

--- Production.Software.API/Program.cs
+++ Production.Software.API/Program.cs
@@ -1,5 +1,11 @@
+using System.Reflection;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Production.Software.Application.Handlers.CommandHandlers;
+using Production.Software.Core.Repositories;
+using Production.Software.Core.Repositories.Base;
 using Production.Software.Infrastructure.Data;
+using Production.Software.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +19,12 @@ builder.Services.AddSwaggerGen();
 //Add AppDbContext
 builder.Services.AddDbContext<AppDbContext>(m => 
     m.UseNpgsql(builder.Configuration.GetConnectionString("Default") ?? throw new InvalidOperationException()), ServiceLifetime.Singleton);
+
+
+builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.AddMediatR(typeof(CreateProductHandler).GetTypeInfo().Assembly);
+builder.Services.AddScoped(typeof(IRepository < > ), typeof(Repository < > ));
+builder.Services.AddTransient < IProductRepository, ProductRepository > ();
 
 var app = builder.Build();
 
