61cccc646281169b417e88a135cb907eefb8fcef
Milad Dehghan <milad.dehghan@hotmail.com>
2022-05-13 00:25:41 +0430

    Product Creation command and response

---
+++ Production.Software.Application/Commands/CreateProductCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using Production.Software.Application.Responses;
+
+namespace Production.Software.Application.Commands;
+
+public class CreateProductCommand : IRequest<ProductResponse> 
+{
+    public string Name { get; set; }
+}
---
+++ Production.Software.Application/Responses/ProductResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace Production.Software.Application.Responses;
+
+public class ProductResponse
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+}
